cd601bbaea0d5a758247bfcdf10d11cbf052b3a6
Ars <asd>
2021-09-15 01:32:45 +0800

    IfElse.kt and Logical.kt done

--- src/lesson2/task2/Logical.kt
+++ src/lesson2/task2/Logical.kt
@@ -3,6 +3,8 @@
 package lesson2.task2
 
 import lesson1.task1.sqr
+import lesson1.task1.trackLength
+import lesson2.task1.rookOrBishopThreatens
 
 /**
  * Пример
@@ -18,7 +20,7 @@ fun pointInsideCircle(x: Double, y: Double, x0: Double, y0: Double, r: Double) =
  * Четырехзначное число назовем счастливым, если сумма первых двух ее цифр равна сумме двух последних.
  * Определить, счастливое ли заданное число, вернуть true, если это так.
  */
-fun isNumberHappy(number: Int): Boolean = TODO()
+fun isNumberHappy(number: Int): Boolean = (number / 1000) + (number / 100 % 10) == (number % 100 / 10) + (number % 10)
 
 /**
  * Простая (2 балла)
@@ -27,7 +29,14 @@ fun isNumberHappy(number: Int): Boolean = TODO()
  * Определить, угрожают ли они друг другу. Вернуть true, если угрожают.
  * Считать, что ферзи не могут загораживать друг друга.
  */
-fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean = TODO()
+fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean {
+    if (x1 == x2 || y1 == y2) return true
+    //Диагонали проверяем как в rookOrBishopThreatens()
+    //Проверяем сумму координат, инвертируем координаты, опять проверяем
+    if (x1 + y1 == x2 + y2) return true
+    if ((22 - x1 - y1) == (22 - x2 - y2)) return true
+    return false
+}
 
 
 /**
@@ -36,7 +45,18 @@ fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean = TODO()
  * Дан номер месяца (от 1 до 12 включительно) и год (положительный).
  * Вернуть число дней в этом месяце этого года по григорианскому календарю.
  */
-fun daysInMonth(month: Int, year: Int): Int = TODO()
+fun daysInMonth(month: Int, year: Int): Int {
+    val isYearLeap = when {
+        year % 100 == 0 -> year % 400 == 0
+        year % 4 == 0 -> true
+        else -> false
+    }
+    return when (month) {
+        1, 3, 5, 7, 8, 10, 12 -> 31
+        4, 6, 9, 11 -> 30
+        else -> if (isYearLeap) 29 else 28
+    }
+}
 
 /**
  * Простая (2 балла)
@@ -48,7 +68,7 @@ fun daysInMonth(month: Int, year: Int): Int = TODO()
 fun circleInside(
     x1: Double, y1: Double, r1: Double,
     x2: Double, y2: Double, r2: Double
-): Boolean = TODO()
+): Boolean = trackLength(x1, y1, x2, y2) + r1 <= r2
 
 /**
  * Средняя (3 балла)
@@ -59,4 +79,8 @@ fun circleInside(
  * кирпич 4 х 4 х 4 пройдёт через отверстие 4 х 4.
  * Вернуть true, если кирпич пройдёт
  */
-fun brickPasses(a: Int, b: Int, c: Int, r: Int, s: Int): Boolean = TODO()
+fun brickPasses(a: Int, b: Int, c: Int, r: Int, s: Int): Boolean {
+    val abc = listOf(a, b, c).sorted()
+    return abc[0] * abc[1] <= r * s
+
+}
