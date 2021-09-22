d75e75275fbe500fb6d59a0367f63b5735ce9883
Ars <asd>
2021-09-23 03:48:33 +0800

    Loop fixed

--- src/lesson4/task1/List.kt
+++ src/lesson4/task1/List.kt
@@ -120,7 +120,8 @@ fun buildSumExample(list: List<Int>) = list.joinToString(separator = " + ", post
  * по формуле abs = sqrt(a1^2 + a2^2 + ... + aN^2).
  * Модуль пустого вектора считать равным 0.0.
  */
-fun abs(v: List<Double>): Double = TODO()
+fun abs(v: List<Double>): Double = sqrt(v.sumOf { it * it })
+//Idea сама это предложила,когда я написал v.map { it * it }.sum()
 
 /**
  * Простая (2 балла)
