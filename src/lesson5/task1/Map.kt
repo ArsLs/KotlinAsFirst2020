44c9d698138e47b56dc51942764ad4a1d4715f9c
Ars <asd>
2021-11-01 13:45:06 +0300

    UwU

--- src/lesson5/task1/Map.kt
+++ src/lesson5/task1/Map.kt
@@ -195,7 +195,13 @@ fun mergePhoneBooks(mapA: Map<String, String>, mapB: Map<String, String>): Map<S
  *   averageStockPrice(listOf("MSFT" to 100.0, "MSFT" to 200.0, "NFLX" to 40.0))
  *     -> mapOf("MSFT" to 150.0, "NFLX" to 40.0)
  */
-fun averageStockPrice(stockPrices: List<Pair<String, Double>>): Map<String, Double> = TODO()
+fun averageStockPrice(stockPrices: List<Pair<String, Double>>): Map<String, Double> {
+    var result = mutableMapOf<String, Double>()
+    for ((stock, price) in stockPrices) {
+        if (result[stock] == null) result[stock] = price else result[stock] = (result[stock]!! + price) / 2
+    }
+    return result
+}
 
 /**
  * Средняя (4 балла)
