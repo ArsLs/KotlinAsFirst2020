730d931bea9325b494b0aeaf61e594569f18a9f0
Ars <asd>
2021-12-12 16:25:27 +0300

    MarkdownToHtmlSimple

--- src/lesson7/task1/Files.kt
+++ src/lesson7/task1/Files.kt
@@ -321,11 +321,12 @@ fun markdownToHtmlSimple(inputName: String, outputName: String) {
     val writer = File(outputName).bufferedWriter()
     writer.write("<html><body><p>")
     var text = File(inputName).readText()
-    text = Regex("""\*\*[^(**)]+\*\*""").replace(text) { m: MatchResult ->
+
+    text = Regex("""\*\*[^*]+\*\*""").replace(text) { m: MatchResult ->
         "<b>" + m.value.drop(2).dropLast(2) + "</b>"
     }
-    text = Regex("""\*[^*]+\*""").replace(text) { m: MatchResult ->
-        "<i>" + m.value.drop(1).dropLast(1) + "</i>"
+    text = Regex("""[^*]\*[^*]+\*[^*]""").replace(text) { m: MatchResult ->
+        m.value.take(1) + "<i>" + m.value.drop(2).dropLast(2) + "</i>" + m.value.takeLast(1)
     }
     text = Regex("""~~[^~]+~~""").replace(text) { m: MatchResult ->
         "<s>" + m.value.drop(2).dropLast(2) + "</s>"
